2100b7f2384b500aa99f1f8bdc9111b5ba54151a
nzinovev <nervous9339@gmail.com>
2021-10-01 19:58:04 +0300

    additional info added for todo's

--- src/main/java/com/example/configuration/MvcConfiguration.java
+++ src/main/java/com/example/configuration/MvcConfiguration.java
@@ -6,4 +6,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfiguration implements WebMvcConfigurer {
 
     //todo: 1. Add MvcConfiguration. (hint: don't forget about annotations).
+    // You need method addViewController which will register your views
+    // You need ViewResolver bean with info where your view placed (WEB-INF...)
 }
