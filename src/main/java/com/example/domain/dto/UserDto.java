7e64b4fa79cfa0aad5ce5a74afc93994b7c72d7d
nzinovev <nervous9339@gmail.com>
2021-10-29 01:42:22 +0300

    javax validation added

--- src/main/java/com/example/domain/dto/UserDto.java
+++ src/main/java/com/example/domain/dto/UserDto.java
@@ -4,14 +4,28 @@ import com.example.domain.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class UserDto {
 
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String login;
+    @Email
+    @Size(max = 50)
     private String email;
+    @NotBlank
+    @Size(max = 90)
     private int age;
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String password;
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String confirmPassword;
 
     public UserDto() {}
