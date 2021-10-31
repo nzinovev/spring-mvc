81bbd0e3feae5a4b17f43e56d4e4abfb84d89d92
nzinovev <nervous9339@gmail.com>
2021-10-31 18:09:46 +0300

    configuration re-written to 'onStartup' method

--- src/main/java/com/example/configuration/WebApplicationConfig.java
+++ src/main/java/com/example/configuration/WebApplicationConfig.java
@@ -1,21 +1,42 @@
 package com.example.configuration;
 
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class WebApplicationConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        var rootContext = new AnnotationConfigWebApplicationContext();
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        var servletAppContext = new AnnotationConfigWebApplicationContext();
+        servletAppContext.register(SpringWebConfig.class);
+
+        var dispatcherServlet = new DispatcherServlet(servletAppContext);
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+    @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class[0];
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SpringWebConfig.class};
+        return new Class[0];
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[]{"/"};
+        return new String[0];
     }
 }
